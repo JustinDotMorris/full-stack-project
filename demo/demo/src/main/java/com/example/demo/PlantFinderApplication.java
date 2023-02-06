844df6dce45f6c55356823d1de88a41cf01f3461
Justin Morris <85644773+JustinDotMorris@users.noreply.github.com>
2023-02-06 17:31:27 +0000

    Added all basic set up files

---
+++ demo/demo/src/main/java/com/example/demo/PlantFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantFinderApplication.class, args);
+	}
+
+}
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>header</h1>
+      </header>
+      <body>
+        <h2>body</h2>
+      </body>
+      <footer>
+        <h3>footer</h3>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
